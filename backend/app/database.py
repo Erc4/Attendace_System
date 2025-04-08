a11c1863950eb63a5de7b60e972e3cc59163298d
Erc4 <ericamezcua3@gmail.com>
2025-04-08 10:46:43 -0700

    Actualización sin importancia

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root@localhost/AttendanceSystem"
+DATABASE_URL = "mysql+mysqlconnector://root@localhost/attendanceSystem"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
